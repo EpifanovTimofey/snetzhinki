96e9e1513377930ff7007d8b09eb678bcddcf184
artem <Football@gmail.com>
2023-02-04 15:41:19 +0300

    sneg

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import wrap, random
+import wrap, random, time
 
 wrap.world.create_world(700, 600)
 wrap.world.set_back_color(30, 25, 200)
@@ -6,6 +6,7 @@ wrap.add_sprite_dir("sprite")
 snezhinki = []
 text = wrap.sprite.add_text(str(len(snezhinki)), 40, 40, font_size=80)
 s2 = None
+a = time.time()
 
 
 @wrap.always(1000)
@@ -17,7 +18,7 @@ def create_snezhinki():
 
 
 @wrap.always(50)
-def snezhinka(pos_x, pos_y):
+def snezhinka():
     for s in snezhinki:
         cost = wrap.sprite.get_costume(s)
         if cost == "SNOWFLAKE" and s != s2:
@@ -34,7 +35,7 @@ def snezhinka(pos_x, pos_y):
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def voda(pos_x, pos_y):
     for s in snezhinki:
-        if wrap.sprite.is_collide_point(s, pos_x, pos_y, True):
+        if wrap.sprite.is_collide_point(s, pos_x, pos_y, True):  # and s != s2:
             wrap.sprite.set_costume(s, "WATER")
             wrap.sprite.set_angle(s, 90)
             break
@@ -43,35 +44,33 @@ def voda(pos_x, pos_y):
 @wrap.on_mouse_down(wrap.BUTTON_RIGHT)
 def peretaskivanie1(pos_x, pos_y):
     global s2
+    if s2 != None:
+        if wrap.sprite.get_costume(s2) == "snowflake_fioletovay":
+            wrap.sprite.set_costume(s2,"SNOWFLAKE")
+        s2 = None
+        return
     for s in snezhinki:
-        if wrap.sprite.is_collide_point(s, pos_x, pos_y, True) and s2 == s2:
+        if wrap.sprite.is_collide_point(s, pos_x, pos_y, True) and wrap.sprite.get_costume(s) != "WATER":
             s2 = s
-            if wrap.sprite.is_collide_sprite(s2,s):
-                s2 = None
-                return
-        if wrap.sprite.is_collide_point(s, pos_x, pos_y, True) and s2 == s:
-            s2 = None
+            wrap.sprite.move_to(s2, pos_x + 40, pos_y + 40)
             return
-
-
-
-
-
-
-#если игрок нажал и у него есть снежинка, то
-    #отпустить снежинку
-    #return
-#если игрок нажал на снежинку, то
-    #взять снежинку
-    #return
-
-
-
-
 
 
 @wrap.on_mouse_move()
 def peretaskivanie2(pos_x, pos_y):
-    global s2
     if s2 != None:
-        wrap.sprite.move_to(s2, pos_x, pos_y)
+        wrap.sprite.move_to(s2, pos_x + 40, pos_y + 40)
+
+
+@wrap.always()
+def morganie():
+    global a
+    # if s2 != None and time.time() - a > 1:
+    #     wrap.sprite.set_costume(s2, "snowflake_fioletovay")
+    if s2 != None and time.time() - a > wrap.sprite.get_angle(s2)/3:
+        a = time.time()
+        if wrap.sprite.get_costume(s2) == "SNOWFLAKE":
+            wrap.sprite.set_costume(s2, "snowflake_fioletovay")
+            return
+        if wrap.sprite.get_costume(s2) == "snowflake_fioletovay":
+            wrap.sprite.set_costume(s2, "SNOWFLAKE")
