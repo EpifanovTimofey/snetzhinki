3936b93c30cd33e518f51847bc5befef878fb50a
artem <Football@gmail.com>
2023-02-13 19:12:57 +0300

    Sneg

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ player_coin = 0
 s2 = None
 a = time.time()
 coins = 0
+click = 0
+click_sprite = 15
 msn1 = wrap.sprite.add("picture", 590, 50, "SNOWFLAKE")
 msn2 = wrap.sprite.add("picture", 650, 50, "WATER")
 msn3 = wrap.sprite.add("picture", 525, 50, "fire")
 msn4 = wrap.sprite.add("picture", 457, 50, "super_knopka")
 msn5 = wrap.sprite.add("picture", 385, 50, "knopka")
+msn6 = wrap.sprite.add("picture", 305, 50, "veter")
 text_coin1 = wrap.sprite.add("picture", 55, 93, "coin")
 fire_coin1 = wrap.sprite.add("picture", 515, 100, "coin")
 fire_coin2 = wrap.sprite.add("picture", 525, 100, "coin")
 fire_coin3 = wrap.sprite.add("picture", 535, 100, "coin")
+veter_coin1 = wrap.sprite.add("picture", 313, 100, "coin")
 snowflake_coin1 = wrap.sprite.add("picture", 590, 100, "coin")
 water_coin1 = wrap.sprite.add("picture", 645, 100, "coin")
 water_coin2 = wrap.sprite.add("picture", 655, 100, "coin")
@@ -29,16 +33,21 @@ knopka_text1 = wrap.sprite.add_text("4", 377, 100, font_size=25)
 study1 = wrap.sprite.add("picture", 30, 131, "WATER")
 study2 = wrap.sprite.add_text("=", 52, 135, font_size=25)
 study3 = wrap.sprite.add("picture", 74, 136)
+klick_text1 = wrap.sprite.add_text(str(click_sprite), 30, 175, font_size=30)
+klick_sprite1 = wrap.sprite.add("picture", 65, 170, "klick1")
 wrap.sprite.set_size(study1, 25, 35)
 wrap.sprite.set_size(study3, 26, 26)
 
 
 @wrap.always(800)
 def create_snezhinki():
-    s1 = wrap.sprite.add("picture", random.randint(50, 650), -10, "SNOWFLAKE")
-    wrap.sprite.set_angle(s1, random.randint(1, 5))
-    snezhinki.append(s1)
-    wrap.sprite_text.set_text(text, str(len(snezhinki)))
+    if player_coin <= 300:
+        s1 = wrap.sprite.add("picture", random.randint(50, 650), -10, "SNOWFLAKE")
+        wrap.sprite.set_angle(s1, random.randint(1, 5))
+        snezhinki.append(s1)
+        wrap.sprite_text.set_text(text, str(len(snezhinki)))
+    else:
+        wrap.sprite.add_text("Поздравляю! Ты прошёл игру!", random.randint(0, 700), random.randint(0, 600),font_size=50)
 
 
 @wrap.on_mouse_down(wrap.BUTTON_RIGHT)
@@ -102,15 +111,32 @@ def peretaskivanie1(pos_x, pos_y):
                     if coins == 6:
                         coins = 0
                         return
+        if wrap.sprite.is_collide_point(msn6, pos_x, pos_y) and player_coin >= 1:
+            player_coin -= 1
+            wrap.sprite_text.set_text(text1, str(player_coin))
+            for s in snezhinki:
+                x1 = wrap.sprite.get_right(s)
+                if wrap.sprite.get_costume(s) == "SNOWFLAKE":
+                    wrap.sprite.move(s, 30, -8)
+                if wrap.sprite.get_costume(s) == "WATER":
+                    wrap.sprite.move(s, 35, 4)
+                if x1 > 700:
+                    wrap.sprite.move_right_to(s, 700)
 
 
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def voda(pos_x, pos_y):
-    for s in snezhinki:
-        if wrap.sprite.is_collide_point(s, pos_x, pos_y, True):  # and s != s2:
-            wrap.sprite.set_costume(s, "WATER")
-            wrap.sprite.set_angle(s, 90)
-            break
+    global click, click_sprite
+    if click != 15:
+        for s in snezhinki:
+            if wrap.sprite.is_collide_point(s, pos_x, pos_y, True) and wrap.sprite.get_costume(
+                    s) != "WATER":  # and s != s2:
+                wrap.sprite.set_costume(s, "WATER")
+                wrap.sprite.set_angle(s, 90)
+                click += 1
+                click_sprite -= 1
+                wrap.sprite_text.set_text(klick_text1, str(click_sprite))
+                break
 
 
 @wrap.on_mouse_move()
@@ -135,7 +161,7 @@ def morganie():
 
 @wrap.always(50)
 def snezhinka():
-    global player_coin, text1
+    global player_coin, text1, blokirovka
     for s in snezhinki:
         rand = random.randint(1, 50)
         cost = wrap.sprite.get_costume(s)
@@ -156,8 +182,8 @@ def snezhinka():
             wrap.sprite_text.set_text(text, str(len(snezhinki)))
 
 
-@wrap.on_key_always(wrap.K_d)
-def kod():
+@wrap.on_key_down(wrap.K_d)
+def kody():
     global player_coin, text1
     player_coin += 99
     wrap.sprite_text.set_text(text1, str(player_coin))
